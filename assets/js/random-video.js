988a6472171b83826adfa8a35b8baa1f1125be83
Zaquariah Holland <zaquariah@gmail.com>
2021-06-20 11:56:42 -0500

    use video IDs instead

--- assets/js/random-video.js
+++ assets/js/random-video.js
@@ -1,27 +1,40 @@
-const videoPath = 'assets/video/'
-const videoType = '.mp4'
-const videoStorage = [
-    'i_may_not_know_my_flowers',
-    'red_birb',
-    'set_the_record_straight',
-    'update_day',
-    'j_roc_it_could_happen_to_you',
-    'how_to_be_a_slytherin'
+const videoURL = '//www.youtube.com/embed/'
+// youtube embed parameters
+// https://developers.google.com/youtube/player_parameters
+const videoControls = [
+    '?version=3',
+    '&rel=1',
+    '&loop=1',
+    '&autoplay=1',
+    // '&mute=1',
+    '&controls=0',
+    // '&iv_load_policy=3',
+    // '&modestbranding=1',
+    '&playlist=' // this has to go last
+]
+const videoIDs = [
+    'ZL38UKvHhuQ', // I may not know my flowers....
+    'kUJw2eVYznw', // Red birb "GUMI" wuewuewuewuewue.....staring II
+    'RWP4B23OjZI', // just to set the record straight i do wash my clothes
+    'B_qnI1WrlnU', // Update Day - Nirvana The Band The Show
+    'urZukvTwvfQ', // J Roc - It Could Happen To You. - Trailer Park Boys
+    'Bx-bHmSbPJ4', // How to be a Slytherin - Sailor J
 ]
 
 document.onreadystatechange = function () {
     if (document.readyState === "interactive") {
-        video = document.querySelector('video')
-        chosenVideo = videoStorage[Math.round(Math.random() * (videoStorage.length - 1))]
+        video = document.querySelector('iframe')
+        videoChosen = videoIDs[Math.round(Math.random() * (videoIDs.length - 1))]
 
-        video.setAttribute('src', videoPath + chosenVideo + videoType)
+        video.setAttribute('src', videoURL + videoChosen + videoControls.join('') + videoChosen)
     }
 }
 
+// force play of I may not know my flowers....
 window.addEventListener('keydown', (event) => {
     if (event.ctrlKey === true && event.key === 'b') {
-        video = document.querySelector('video')
-        video.setAttribute('src', videoPath + 'i_may_not_know_my_flowers' + videoType)
+        video = document.querySelector('iframe')
+        video.setAttribute('src', videoURL + 'ZL38UKvHhuQ' + videoControls.join('') + videoChosen)
         console.log('forced bitch video')
     }
 })

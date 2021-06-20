2eb4ea674dd28a182b4f63e4521c8d9c529bf14d
Zaquariah Holland <zaquariah@gmail.com>
2021-06-20 11:11:19 -0500

    added forced command

--- assets/js/random-video.js
+++ assets/js/random-video.js
@@ -1,19 +1,27 @@
+const videoPath = 'assets/video/'
+const videoType = '.mp4'
+const videoStorage = [
+    'i_may_not_know_my_flowers',
+    'red_birb',
+    'set_the_record_straight',
+    'update_day',
+    'j_roc_it_could_happen_to_you',
+    'how_to_be_a_slytherin'
+]
+
 document.onreadystatechange = function () {
     if (document.readyState === "interactive") {
-        const videoPath = 'assets/video/'
-        const videoType = '.mp4'
-        const videoStorage = [
-            'i_may_not_know_my_flowers',
-            'red_birb',
-            'set_the_record_straight',
-            'update_day',
-            'j_roc_it_could_happen_to_you',
-            'how_to_be_a_slytherin'
-        ]
-
         video = document.querySelector('video')
         chosenVideo = videoStorage[Math.round(Math.random() * (videoStorage.length - 1))]
 
         video.setAttribute('src', videoPath + chosenVideo + videoType)
     }
 }
+
+window.addEventListener('keydown', (event) => {
+    if (event.ctrlKey === true && event.key === 'b') {
+        video = document.querySelector('video')
+        video.setAttribute('src', videoPath + 'i_may_not_know_my_flowers' + videoType)
+        console.log('forced bitch video')
+    }
+})

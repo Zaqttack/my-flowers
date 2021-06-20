4ff39942a83468f8428236968dc691ca4b5c9db6
Zaquariah Holland <zaquariah@gmail.com>
2021-06-20 11:02:09 -0500

    new videos

--- assets/js/random-video.js
+++ assets/js/random-video.js
@@ -1,17 +1,19 @@
 document.onreadystatechange = function () {
-    let chosenVideo;
-    let video;
-
     if (document.readyState === "interactive") {
         const videoPath = 'assets/video/'
+        const videoType = '.mp4'
         const videoStorage = [
-            'i_may_not_know_my_flowers.mp4',
-            'red_birb.mp4'
+            'i_may_not_know_my_flowers',
+            'red_birb',
+            'set_the_record_straight',
+            'update_day',
+            'j_roc_it_could_happen_to_you',
+            'how_to_be_a_slytherin'
         ]
 
         video = document.querySelector('video')
         chosenVideo = videoStorage[Math.round(Math.random() * (videoStorage.length - 1))]
 
-        video.setAttribute('src', videoPath + chosenVideo)
+        video.setAttribute('src', videoPath + chosenVideo + videoType)
     }
 }
